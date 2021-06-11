601d2071f48b385227cce2a7c17223b252bc16ce
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-06-11 16:36:36 +0200

    Fixed indications of where/how to edit, added rules to allow SSH (Vagrant provisioner)

---
+++ vagrant box/iptables.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Flush
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+# Block All
+iptables -P OUTPUT DROP
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+
+# allow Localhost
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Make sure you can communicate with any DHCP server
+iptables -A OUTPUT -d 255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 255.255.255.255 -j ACCEPT
+
+# EDIT THE FOLLOWING TWO LINES. Make sure that you can communicate within your own network
+iptables -A INPUT -s **USE_LOCAL_NETWORK_IP_AND_SUBNET_MASK_IN_CIDR_NOTATION** -d **USE_LOCAL_NETWORK_IP_AND_SUBNET_MASK_IN_CIDR_NOTATION** -j ACCEPT
+iptables -A OUTPUT -s **USE_LOCAL_NETWORK_IP_AND_SUBNET_MASK_IN_CIDR_NOTATION** -d **USE_LOCAL_NETWORK_IP_AND_SUBNET_MASK_IN_CIDR_NOTATION** -j ACCEPT
+
+# Allow established sessions to receive traffic:
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow TUN
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A FORWARD -i tun+ -j ACCEPT
+iptables -A FORWARD -o tun+ -j ACCEPT
+iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+iptables -A OUTPUT -o tun+ -j ACCEPT
+
+# allow VPN connection
+iptables -I OUTPUT 1 -p udp --destination-port 1194 -m comment --comment "Allow VPN connection" -j ACCEPT
+iptables -I OUTPUT 2 -p udp --destination-port 53 -m comment --comment "Allow DNS" -j ACCEPT
+
+# EDIT THE FOLLOWING LINE. Allow SSH from/to local network. Needed by Vagrant to provision VM
+iptables -A INPUT -p tcp -s **USE_LOCAL_NETWORK_IP_AND_SUBNET_MASK_IN_CIDR_NOTATION** --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Block All
+iptables -A OUTPUT -j DROP
+iptables -A INPUT -j DROP
+iptables -A FORWARD -j DROP
+
+# Log all dropped packages, debug only.
+
+#iptables -N logging
+#iptables -A INPUT -j logging
+#iptables -A OUTPUT -j logging
+#iptables -A logging -m limit --limit 2/min -j LOG --log-prefix "IPTables general: " --log-level 7
+#iptables -A logging -j DROP
+
+echo "saving"
+iptables-save > /etc/iptables.rules
+echo "done"
+#echo 'openVPN - Rules successfully applied, we start "watch" to verify IPtables in realtime (you can cancel it as usual CTRL + c)'
+#sleep 3
+#watch -n 0 "sudo iptables -nvL"
