95b639cf90a5ec442038c2b29d35dfbbedfb2ce0
MSchotsman <matthijs_schotsman@hotmail.nl>
2021-06-24 15:08:23 +0200

    Moved block_all into init

--- client/init.sh
+++ client/init.sh
@@ -6,8 +6,24 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-MAC="$1"
-ROUTER="${MAC//-/:}"
+# Fill in the router's mac address
+ROUTER="6B:9C:8A:24:A5:7B"
 
+# Could also include a port number if neccessary
+# Fill in the mac address of the machine that runs Vagrant
+VAGRANT="14:33:f3:0d:eb:03"
+
+
+# Allow the router to send packets
 arptables -A OUTPUT --source-mac $ROUTER -j ACCEPT
 arptables -A INPUT --destination-mac $ROUTER -j ACCEPT
+
+# Allow ssh for Vagrant
+arptables -A OUTPUT --source-mac $VAGRANT -j ACCEPT
+arptables -A INPUT --destination-mac $VAGRANT -j ACCEPT
+
+# Change all policies to drop all packets
+arptables -P INPUT DROP
+arptables -P OUTPUT DROP
+arptables -P FORWARD DROP
+
