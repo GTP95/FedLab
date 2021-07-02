5f4637d6636c0e131ac862751c3649e2a11ae905
M.D.V. Meijer <maxm@hotmail.nl>
2021-07-02 21:22:36 +0200

    clean up file

--- client/vagrant box/iptables.sh
+++ client/vagrant box/iptables.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Flush
+# flush iptables
 iptables -t nat -F
 iptables -t mangle -F
 iptables -F
 iptables -X
 
-# allow Localhost
+# allow connections from localhost
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Make sure you can communicate with any DHCP server
+# allow DHCP server connections
 iptables -A OUTPUT -d 255.255.255.255 -j ACCEPT
 iptables -A INPUT -s 255.255.255.255 -j ACCEPT
 
@@ -17,10 +17,10 @@ iptables -A INPUT -s 255.255.255.255 -j ACCEPT
 iptables -A INPUT -s 192.168.1.0/24 -d 192.168.1.0/24 -j ACCEPT
 iptables -A OUTPUT -s 192.168.1.0/24 -d 192.168.1.0/24 -j ACCEPT
 
-# Allow established sessions to receive traffic:
+# allow established sessions to receive traffic
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Allow TUN
+# allow TUN such that the VPN connection works
 iptables -A INPUT -i tun+ -j ACCEPT
 iptables -A FORWARD -i tun+ -j ACCEPT
 iptables -A FORWARD -o tun+ -j ACCEPT
@@ -35,18 +35,6 @@ iptables -I OUTPUT 2 -p udp --destination-port 53 -m comment --comment "Allow DN
 iptables -A INPUT -p tcp -s 192.168.1.0/24 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-
-# Log all dropped packages, debug only.
-
-#iptables -N logging
-#iptables -A INPUT -j logging
-#iptables -A OUTPUT -j logging
-#iptables -A logging -m limit --limit 2/min -j LOG --log-prefix "IPTables general: " --log-level 7
-#iptables -A logging -j DROP
-
 echo "saving"
 iptables-save > /etc/iptables.rules
 echo "done"
-#echo 'openVPN - Rules successfully applied, we start "watch" to verify IPtables in realtime (you can cancel it as usual CTRL + c)'
-#sleep 3
-#watch -n 0 "sudo iptables -nvL"
