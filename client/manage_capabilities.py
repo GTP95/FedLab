4fe6ec63dce6b3c57ce56ffe9b400340608e7fc4
M.D.V. Meijer <maxm@hotmail.nl>
2021-11-12 12:02:18 +0100

    convert to new JSON formats according to the requirements

--- client/manage_capabilities.py
+++ client/manage_capabilities.py
@@ -6,13 +6,15 @@
 # {
 #   'capabilities': [
 #     {
-#       "device": "11:22:33:44:55:66",  // use only a plain text name here or MAC / another ID?
+#       "device": "192.168.5.58",  // use only a plain text name here or MAC / another ID?
+#       "port": "1024",
 #       "capability_name": "very cool hack",
 #       "capability_id": "9f96d940-ea30-472e-99d5-d2dc9f45bbba",  // just a UUID, or maybe we can hash some data of the capability?
 #       "description": "hack that destroys space and time"
 #     },
 #     {
-#       "device": "22:33:44:55:66:77",  // use only a plain text name here or MAC / another ID?
+#       "device": "192.168.5.231",  // use only a plain text name here or MAC / another ID?
+#       "port": "1028",
 #       "capability_name": "monitor voice commands",
 #       "capability_id": "9f96d940-ea30-472e-99d5-d2dc9f45bbbb",  // just a UUID, or maybe we can hash some data of the capability?
 #       "description": "monitor voice commands to invade people's privacy (get permission first pls)"
@@ -23,6 +25,7 @@
 import json
 import os
 import uuid
+from rules_and_port_generator import generatePortAndAddRules
 
 
 # 2 weeks
@@ -30,6 +33,7 @@ TTL = 14*24*60*60
 SERVER = "10.0.2.2"
 PORT = 1883
 OPENVPN_CLIENT_NAME = "INTERSECT2"  # configure this
+GATEWAY_IP = "192.168.5.1"  # configure this
 
 
 class MqttCapabilityUpdateManager:
@@ -72,47 +76,53 @@ def construct_empty_directory():
         f.write(json.dumps({"capabilities": []}))
 
 
-def construct_local_capability_object(mac, name, desc):
+def construct_local_capability_object(ip, gateway_port, device_port, name, desc):
     return {
-           "device": mac,
+           "device": ip,
+           "gateway_port": gateway_port,
+           "device_port": device_port,
            "capability_name": name,
-           "capability_id": str(uuid.uuid4()),
            "description": desc
          }
 
 
-def construct_remote_capability_object(mac, name, desc):
+def construct_remote_capability_object(name, desc, port):
     return {
            "party_name": OPENVPN_CLIENT_NAME,
-           "device": mac,
+           "gateway_ip": GATEWAY_IP,
+           "port": port,
            "capability_name": name,
-           "capability_id": str(uuid.uuid4()),
            "description": desc
          }
 
 
-def add_capability(mac, name, desc):
+def add_capability(ip, device_port, name, desc):
     cap_directory = read_directory()
 
-    new_capability = construct_local_capability_object(mac, name, desc)
+    gateway_port = generatePortAndAddRules(ip, device_port)
+
+    new_capability = construct_local_capability_object(
+        ip, gateway_port, device_port, name, desc)
     cap_directory["capabilities"].append(new_capability)
 
-    remote_capability = construct_remote_capability_object(mac, name, desc)
+    remote_capability = construct_remote_capability_object(
+        name, desc, gateway_port)
     mqtt_client.publish_capability_update("add", json.dumps(remote_capability))
 
     write_directory(cap_directory)
 
 
 def remove_capability(uuid):
-    cap_directory = read_directory()
-    print(cap_directory['capabilities'])
-
-    # only keep items that do not have the specified UUID
-    cap_directory['capabilities'] = list(filter(
-        lambda x: x["capability_id"] != uuid,
-        cap_directory['capabilities'])
-    )
-
-    mqtt_client.publish_capability_update("remove", uuid)
-
-    write_directory(cap_directory)
+    # TODO: fix this, since ID is no longer there
+    # cap_directory = read_directory()
+    # print(cap_directory['capabilities'])
+    #
+    # # only keep items that do not have the specified UUID
+    # cap_directory['capabilities'] = list(filter(
+    #     lambda x: x["capability_id"] != uuid,
+    #     cap_directory['capabilities'])
+    # )
+    #
+    # mqtt_client.publish_capability_update("remove", uuid)
+    #
+    # write_directory(cap_directory)
