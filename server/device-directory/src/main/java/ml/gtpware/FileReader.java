8529d3f0299d2eff30dc6986c5ab104d22233ff4
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-11-14 13:13:54 +0100

    Removed unused code

--- server/device-directory/src/main/java/ml/gtpware/FileReader.java
+++ server/device-directory/src/main/java/ml/gtpware/FileReader.java
@@ -2,13 +2,11 @@ package ml.gtpware;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
+
 
 public class FileReader {
     private Path filePath;
@@ -16,8 +14,6 @@ public class FileReader {
     private SortedSet<Capability> capabilitiesSet;    //Using a set to automatically avoid duplicates
     private static FileReader instance;
     private final Gson gson;
-    private static final Type CapabilitiesList = new TypeToken<List<Capability>>() {
-    }.getType();
 
     private FileReader(){
         this.prettyFormattedDeviceDirectoryContent="Waiting for data";
@@ -50,7 +46,6 @@ public class FileReader {
         System.out.println(jsonStringsArray.length);
         for(int index=0; index<jsonStringsArray.length-1;index++){  //I have to skip the last element as it doesn't contain a json, artifact of string splitting
             jsonStringsArray[index]+="}";   //Splitting a string like above removes terminating '}', have to add this again
-            //json.concat("}vdvdsv"); //Splitting the string like above removes the closing bracket
             System.out.println(jsonStringsArray[index]);
             capabilitiesSet.add(gson.fromJson(jsonStringsArray[index], Capability.class));
         }
