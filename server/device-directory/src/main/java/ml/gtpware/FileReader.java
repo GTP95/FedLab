8d86710924120f797114278f0cdd9d8968c5f65c
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-11-14 13:07:56 +0100

    Improved interface of the device directory, now capabilities are grouped by party

--- server/device-directory/src/main/java/ml/gtpware/FileReader.java
+++ server/device-directory/src/main/java/ml/gtpware/FileReader.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
@@ -14,7 +13,7 @@ import com.google.gson.reflect.TypeToken;
 public class FileReader {
     private Path filePath;
     private String fileContent, prettyFormattedDeviceDirectoryContent;
-    private HashSet<Capability> capabilitiesSet;    //Using a set to automatically avoid duplicates
+    private SortedSet<Capability> capabilitiesSet;    //Using a set to automatically avoid duplicates
     private static FileReader instance;
     private final Gson gson;
     private static final Type CapabilitiesList = new TypeToken<List<Capability>>() {
@@ -22,7 +21,7 @@ public class FileReader {
 
     private FileReader(){
         this.prettyFormattedDeviceDirectoryContent="Waiting for data";
-        this.capabilitiesSet=new HashSet<>();
+        this.capabilitiesSet=new TreeSet<>();
         this.gson=new Gson();
         this.filePath=Path.of("/server-subscriber/device_directory");
         try {
@@ -58,7 +57,19 @@ public class FileReader {
 
         //Present the data in a "pretty" way
         prettyFormattedDeviceDirectoryContent=new String();
-        for(Capability capability : capabilitiesSet){
+        Iterator iterator=capabilitiesSet.iterator();
+        Capability previousCapability=capabilitiesSet.first();
+        Capability capability;
+        prettyFormattedDeviceDirectoryContent+="Party: " + previousCapability.party_name + "\n";
+        while(iterator.hasNext()){
+            capability= (Capability) iterator.next();
+            if(capability.party_name.equals(previousCapability.party_name)){
+                prettyFormattedDeviceDirectoryContent+=("\tGateway IP: " + capability.gateway_ip + "\n" +
+                                                        "\tGateway port: " + capability.port + "\n" +
+                                                        "\tCapability name: " + capability.capability_name + "\n" +
+                                                        "\tCapability description: " + capability.description + "\n\n\n");
+            }
+            else
             prettyFormattedDeviceDirectoryContent+=("Party: " + capability.party_name + "\n" +
                                                          "\tGateway IP: " + capability.gateway_ip + "\n" +
                                                          "\tGateway port: " + capability.port + "\n" +
