5c1207c35339f6db8b3c7b92bb3f5d2a357ce538
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-11-18 16:19:36 +0100

    Fixed rule inside rules_and_port_generator.py to use gateway's internal IP and not device's IP in postrouting rule

--- client/rules_and_port_generator.py
+++ client/rules_and_port_generator.py
@@ -6,6 +6,7 @@ import os
 import sys
 import subprocess
 import random
+from netifaces import interfaces, ifaddresses, AF_INET
 
 
 def isAvailable(port):
@@ -34,10 +35,11 @@ def generatePort():
 
 
 def addIptablesRules(ipAddress, devicePort, externalPort):
+	address=netifaces.ifaddresses('enp0s8')[netifaces.AF_INET][0]['addr']	#get gateway's internal address	
 	os.system("sudo iptables -t nat -A PREROUTING -p tcp --dport " + str(externalPort)
 	          + " -j DNAT --to-destination " + str(ipAddress) + ":" + str(devicePort))
 	os.system("sudo iptables -t nat -A POSTROUTING -p tcp --dport "
-	          + str(devicePort) + " -j SNAT --to-source " + str(ipAddress))
+	          + str(devicePort) + " -j SNAT --to-source " + address)
 	#Note: assuming that "echo 1 > /proc/sys/net/ipv4/ip_forward" and "/proc/sys/net/ipv4/conf/eth0/forwarding" do basically the same so not enabling
 	#forwarding here as already enabled in Vagrantfile with "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
