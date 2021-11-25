1f2459ec8aa19f8c36c665013003cfadf59d4fc3
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-11-25 22:37:43 +0100

    Added jar, added iptables config inside script

--- client/get_subnet_from_netbox.py
+++ client/get_subnet_from_netbox.py
@@ -6,6 +6,8 @@ import ipaddress
 import os
 
 token='e3df244fb16186e8aa071e0e430652dcebf6d821'
+IP_RANGE=""
+
 
 if len(sys.argv)!=2:
 	print("Usage: "+ sys.argv[0] + " party_name")
@@ -15,7 +17,7 @@ partyName=sys.argv[1]
 r=requests.get('http://131.155.69.149:8000/api/ipam/ip-ranges/', headers={'Authorization': 'Token '+token, 'Accept': 'application/json; indent=4'})
 for ipRange in r.json()['results']:
 	if ipRange['tenant']['slug']==partyName:
-		
+		IP_RANGE=ipRange['start_address']
 		network=ipaddress.ip_network(ipRange['start_address'])
 		netmask=str(network.netmask)
 		subnetIP=str(network.network_address)
@@ -26,7 +28,11 @@ for ipRange in r.json()['results']:
 
 lastIP=str(lastIP)
 
+#Configure the DHCP server
 os.system("sed -i '13s/192.168.5.0/"+subnetIP+"/'"+" dhcpd.conf")
 os.system("sed -i '13s/255.255.255.0/"+netmask+"/'"+" dhcpd.conf")
 os.system("sed -i '14s/192.168.5.2/"+firstIP+"/'"+" dhcpd.conf")
-os.system("sed -i '14s/192.168.5.255/"+lastIP+"/'"+" dhcpd.conf")		
+os.system("sed -i '14s/192.168.5.255/"+lastIP+"/'"+" dhcpd.conf")
+
+#Configure iptables
+os.system("sudo bash iptables.sh "+IP_RANGE)
