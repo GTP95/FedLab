26994cd8527d674c0a8df4dba3a0c6f3bb9e89a8
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-11-25 15:48:58 +0100

    Added script for autoconfiguration of DHCP server and modified Vagrantfile to remove most of the configuration from there (now only VM_IP needs to be manually configured)

---
+++ client/get_subnet_from_netbox.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+import requests
+import sys
+import ipaddress
+import os
+
+token='e3df244fb16186e8aa071e0e430652dcebf6d821'
+
+if len(sys.argv)!=2:
+	print("Usage: "+ sys.argv[0] + " party_name")
+	sys.exit()
+
+partyName=sys.argv[1]
+r=requests.get('http://131.155.69.149:8000/api/ipam/ip-ranges/', headers={'Authorization': 'Token '+token, 'Accept': 'application/json; indent=4'})
+for ipRange in r.json()['results']:
+	if ipRange['tenant']['slug']==partyName:
+		
+		network=ipaddress.ip_network(ipRange['start_address'])
+		netmask=str(network.netmask)
+		subnetIP=str(network.network_address)
+		iterator=network.hosts()
+		firstIP=str(next(iterator))
+		for ip in iterator:	#Look what I'm forced to do to get the last valid host IP address..
+			lastIP=ip
+
+lastIP=str(lastIP)
+
+os.system("sed -i '13s/192.168.5.0/"+subnetIP+"/'"+" dhcpd.conf")
+os.system("sed -i '13s/255.255.255.0/"+netmask+"/'"+" dhcpd.conf")
+os.system("sed -i '14s/192.168.5.2/"+firstIP+"/'"+" dhcpd.conf")
+os.system("sed -i '14s/192.168.5.255/"+lastIP+"/'"+" dhcpd.conf")		
