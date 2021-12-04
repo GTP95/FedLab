7502bbd7474f6ce5206508b13a8a7cd6846dab7c
Giacomo <giacomo.tom.petrucci@tiscali.it>
2021-12-04 12:47:56 +0100

    Updated configurations for the new super duper device directory

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python /server-subscriber/server_subscriber.py -s mqtt -p 1883 & java -jar /server-subscriber/device_directory.jar & service apache2 start
-
+java -jar /server-subscriber/device_directory.jar
